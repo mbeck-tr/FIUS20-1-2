21580dd4ad8aef68db44efe0c0d70158a3477796
mbeck-tr <mbeck@lutz-und-grub.de>
2020-09-02 11:58:22 +0200

    kopfgesteuerte Schleife

---
+++ SchleifenKopfgesteuert/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchleifenKopfgesteuert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kopfgesteuerte Schleife
+            //Bedingungsprüfung zu Beginn
+            int zahl = 4;
+            
+            while (zahl < 10) // Bedingung muss true sein
+            {
+                Console.WriteLine("Schleifenanweisung: {0}", zahl);
+                zahl++;
+                if (zahl == 8)
+                {
+                    Console.WriteLine("Sonderfall");
+                    continue; // springt direkt zur Bedingungsprüfung der Schleife
+                }
+                Console.WriteLine("Zahl inkrementiert!");
+            }
+            Console.WriteLine("Schleife beendet");
+            Console.ReadLine();
+        }
+    }
+}
