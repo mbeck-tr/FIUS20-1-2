b9be58e35ec30bd03274bf774fc5d62d782c8c3a
mbeck-tr <mbeck@lutz-und-grub.de>
2020-09-02 10:45:15 +0200

    zweiseitige und mehrfache Auswahl

---
+++ VerzweigungMehrfach/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerzweigungMehrfach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mehrfachauswahl
+            bool stammkunde = true;
+            bool neukunde = false;
+
+            if (neukunde)
+            {
+                /* Im Falle eines Neukunden*/
+                Console.WriteLine("Neukunde");
+            }
+            else if (stammkunde)
+            {
+                /* Im Falle eines Stammkunden*/
+                Console.WriteLine("Stammkunde");
+            }
+            else
+            {
+                /* In allen anderen Fällen*/
+                Console.WriteLine("Kein Neu- oder Stammkunde");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VerzweigungenZweiseitig/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerzweigungenZweiseitig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //zweiseitige Auswahl
+            bool stammkunde = true;
+            
+            if (stammkunde) /*Bedingung*/
+                 /*im Falle von Bedingung ist wahr*/
+            {
+                Console.WriteLine("ist Stammkunde");
+                Console.WriteLine("Rabatt gewähren!!!");
+            }
+            else /*im Falle von Bedingung ist falsch*/
+                Console.WriteLine("ist kein Stammkunde");
+
+            Console.WriteLine("Gesamtbetrag: XY Euro");
+            Console.ReadLine();
+        }
+    }
+}
