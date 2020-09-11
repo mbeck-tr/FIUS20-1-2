4b4a15abca9f165bfe4f75370baf9abfe240b93a
Michael Beck <mbeck@lutz-und-grub.de>
2020-09-11 09:48:18 +0200

    Projekt statischeMember hinzugefügt

---
+++ statischeMember/Mathe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statischeMember
+{
+    static class Mathe
+    {
+        public static double PI = 3.14;
+        public static double Addiere(double zahl1, double zahl2)
+        {
+            double summe = zahl1 + zahl2;
+            return summe;
+        }
+        public static double Umfang(double r)
+        {
+            return 2 * PI * r;
+        }
+    }
+}
---
+++ statischeMember/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statischeMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mathe myMatheObject = new Mathe();
+            Console.WriteLine("zahl1: ");
+            string eingabe = Console.ReadLine();
+            double a = Convert.ToDouble(eingabe);
+
+            Console.WriteLine("zahl2: ");
+            eingabe = Console.ReadLine();
+            double b = Convert.ToDouble(eingabe);
+
+            Console.WriteLine("radius: ");
+            eingabe = Console.ReadLine();
+            double radius = Convert.ToDouble(eingabe);
+
+            double ergebnis = Mathe.Addiere(a, b);
+            Console.WriteLine("Ergebnis: " + ergebnis);
+
+            double umfang = 2 * radius * Mathe.PI;
+            umfang = Mathe.Umfang(radius);
+            Console.WriteLine("Umfang: " + umfang);
+
+            Console.ReadLine();
+        }
+
+       
+    }
+
+
+}
